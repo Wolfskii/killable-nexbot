76c43a4b879e61963bd815258fbb5c5f6df775dc
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-03-01 00:43:36 +0100

    Refine code and add comments where missing

--- killable-nextbot/lua/entities/killable-nextbot.lua
+++ killable-nextbot/lua/entities/killable-nextbot.lua
@@ -1,124 +1,157 @@
 AddCSLuaFile()
 
+-- https://wiki.facepunch.com/gmod/NextBot_NPC_Creation
+-- https://www.youtube.com/watch?v=tda_7yimmNQ
+
 ENT.Base = "base_nextbot"
 ENT.Spawnable = true
 
--- Initialize the NPC
+-- Model for the NextBot
+ENT.Model = "models/player/combine_soldier.mdl"
+
+-- Health for the NextBot
+ENT.Health = 100
+
+-- Speed for the NextBot
+ENT.Speed = 100
+
+-- Damage for the NextBot's attack
+ENT.Damage = 10
+
+-- Name of the NextBot for the spawn menu
+ENT.PrintName = "My NextBot"
+
+-- Category for the spawn menu
+ENT.Category = "My NextBot Category"
+
+-- Initializes the NextBot
 function ENT:Initialize()
-    self:SetModel("models/player/zombie_classic.mdl")
-    self.health = 100
+    self:SetModel(self.Model)
+    self:SetHealth(self.Health)
+    self:SetMaxHealth(self.Health)
+    self:SetSpeed(self.Speed)
+    self:SetDamage(self.Damage)
+    self:SetSolid(SOLID_BBOX)
+    self:SetCollisionGroup(COLLISION_GROUP_PLAYER)
 
-    -- Set the step height for the NPC's movement
     if SERVER then
-        self.loco:SetStepHeight(18)
+        self:SetUseType(SIMPLE_USE)
     end
 
-    -- Set the maximum height from which the NPC can drop to its death
-    self.loco:SetDeathDropHeight(1000)
+    self.loco:SetDeathDropHeight(1000) -- How far to fall before death
 
-    -- Set the acceleration and deceleration of the NPC's movement
-    self.loco:SetAcceleration(900)
-    self.loco:SetDeceleration(1200)
-
-    -- Set the jump height of the NPC
-    self.loco:SetJumpHeight(58)
-
-    -- Set the maximum yaw rate of the NPC's movement
-    self.loco:SetMaxYawRate(360)
-
-    -- Set the desired speed of the NPC's movement
-    self.loco:SetDesiredSpeed(100)
-
-    -- Set the gravity of the NPC
-    self.loco:SetGravity(1)
-
-    -- Set the collision bounds of the NPC
-    self:SetCollisionBounds(Vector(-16, -16, 0), Vector(16, 16, 72))
-
-    -- Set the health of the NPC
-    self:SetHealth(self.health)
-
-    -- Set the maximum health of the NPC on the server
-    if SERVER then
-        self:SetMaxHealth(self.health)
-    end
-
-    -- Set the time until the NPC makes an idle sound
-    self.NextIdleSoundTime = CurTime() + math.random(10, 20)
+    self.NextIdleSoundTime = 0
 end
 
--- Handle what happens when the NPC is injured
-function ENT:OnInjured(dmginfo)
-    -- Decrement the NPC's health by the amount of damage inflicted
-    self.health = self.health - dmginfo:GetDamage()
-    self:SetHealth(self.health)
+-- Draws the health bar above the NextBot's head
+function DrawHealthBar(ent, percent)
+    local pos = ent:GetPos() + Vector(0, 0, 80)
+    local ang = LocalPlayer():EyeAngles()
+    ang:RotateAroundAxis(ang:Forward(), 90)
+    ang:RotateAroundAxis(ang:Right(), 90)
 
-    -- If the NPC's health reaches zero or less, handle its death
-    if self.health <= 0 then
-        self:OnKilled()
-    end
+    local width = 100
+    local height = 10
+    local border = 1
+
+    -- Draw the background
+    draw.RoundedBox(4, pos - ang:Up() * (height / 2) - ang:Right() * (width / 2), width, height, Color(50, 50, 50, 255))
+
+    -- Draw the health bar
+    draw.RoundedBox(4, pos - ang:Up() * (height / 2) - ang:Right() * (width / 2) + Vector(border, border, 0), (width - border * 2) * percent, height - border * 2, Color(255 - 255 * percent, 255 * percent, 0, 255))
 end
 
--- Handle what happens when the NPC is killed
-function ENT:OnKilled()
-    -- Create a ragdoll of the NPC
-    self:BecomeRagdoll(dmginfo)
+-- Draws the NextBot
+function ENT:Draw()
+    self:DrawModel()
+
+    -- Draw the health bar
+    local percent = self:Health() / self:GetMaxHealth()
+    DrawHealthBar(self, percent)
 end
 
--- Handle the NPC's behavior
-function ENT:RunBehaviour()
-    -- Loop indefinitely
-    while true do
-        -- If the NPC has an enemy, chase it
-        if self.enemy then
-            self:ChaseEnemy()
-        -- If the NPC doesn't have an enemy, search for one
-        else
-            self:SearchEnemy()
+-- Moves the NextBot
+function ENT:MoveToPos(pos)
+    local path = Path("Follow")
+    path:SetMinLookAheadDistance(300)
+    path:SetGoalTolerance(20)
+    path:Compute(self, pos)
+
+    if not path:IsValid() then return "failed" end
+
+    while path:IsValid() do
+        if path:GetAge() > 1 then
+            path:Compute(self, pos)
         end
 
-        -- Yield control until the next frame
+        path:Update(self)
+
         coroutine.yield()
     end
+
+    return "ok"
 end
 
--- Search for an enemy within the NPC's search radius
-function ENT:SearchEnemy()
-    -- Find all players within our vision cone
-    local targets = ents.FindInCone(self:GetPos(), self:GetForward(), self.SearchDistance, self.SearchAngle)
+-- Called when the NextBot sees an enemy
+function ENT:OnSeenEnemy(enemy)
+    self:SetEnemy(enemy)
 
-    local nearestTarget = nil
-    local nearestTargetDistance = math.huge
+    self:StartActivity(ACT_RUN)
+    self.loco:SetDesiredSpeed(self.Speed)
 
-    for _, target in pairs(targets) do
-        if target:IsPlayer() and target:Alive() then
-            -- Calculate the distance to the target
-            local distanceToTarget = self:GetPos():DistToSqr(target:GetPos())
+    coroutine.wrap(function()
+        while self:HasEnemy() do
+            self:ChaseEnemy()
 
-            -- Check if the target is the nearest we've seen so far
-            if distanceToTarget < nearestTargetDistance then
-                nearestTarget = target
-                nearestTargetDistance = distanceToTarget
-            end
+            coroutine.yield()
         end
-    end
 
-    if nearestTarget then
-        self:SetEnemy(nearestTarget)
-        self:AlertAllies(nearestTarget)
-
-        -- Call OnFoundEnemy callback if it exists
-        if self.OnFoundEnemy then
-            self:OnFoundEnemy(nearestTarget)
-        end
-    end
-
-    return nearestTarget
+        self:StopMoving()
+    end)()
 end
 
--- Add your bot to the NPC spawn tab
-list.Set( "NPC", "my_nextbot", {
-	Name = "My NextBot",
-	Class = "my_nextbot",
-	Category = "My NextBot Category"
+-- Called when the NextBot loses sight of the enemy
+function ENT:OnLostEnemy()
+    self:SetEnemy(nil) -- Clear the enemy
+end
+
+function ENT:OnKilled(damageinfo)
+    self:BecomeRagdoll(damageinfo) -- Create a ragdoll and remove the entity
+end
+
+-- Override the base entity functions to draw the health above the nextbot
+function ENT:Draw()
+    self:DrawModel() -- Draw the model
+end
+
+function ENT:DrawTranslucent()
+    self:Draw() -- Draw the model
+    self:DrawBar() -- Draw the health bar
+end
+
+function ENT:DrawBar()
+    local pos = self:GetPos() -- Get the position of the nextbot
+    pos.z = pos.z + 80 -- Offset the position to draw the health bar above the nextbot
+    local maxHealth = self:GetMaxHealth() -- Get the maximum health of the nextbot
+    local health = self:Health() -- Get the current health of the nextbot
+    local width = 100 -- Set the width of the health bar
+    local height = 10 -- Set the height of the health bar
+    local border = 2 -- Set the width of the border of the health bar
+    local bgPos = Vector(pos.x - width / 2, pos.y - height / 2, pos.z) -- Calculate the position of the background of the health bar
+    local healthPos = Vector(pos.x - width / 2 + border, pos.y - height / 2 + border, pos.z) -- Calculate the position of the health bar
+
+    -- Draw the background of the health bar
+    surface.SetDrawColor(255, 0, 0, 255)
+    surface.DrawOutlinedRect(bgPos.x, bgPos.y, width, height)
+
+    -- Draw the health bar
+    surface.SetDrawColor(0, 255, 0, 255)
+    surface.DrawRect(healthPos.x, healthPos.y, width * (health / maxHealth) - border * 2, height - border * 2)
+end
+
+-- Add the nextbot to the NPC spawn menu
+list.Set("NPC", "simple_nextbot", {
+    Name = "Simple Bot",
+    Class = "simple_nextbot",
+    Category = "Nextbot"
 })
