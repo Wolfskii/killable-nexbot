e01e80ffb4e9eaf1b5a02b41472ee0672df2072d
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-02-28 20:24:31 +0100

    Refine code and add comments where missing

--- addons/nextbot/lua/entities/killable-nextbot.lua
+++ addons/nextbot/lua/entities/killable-nextbot.lua
@@ -1,198 +1,117 @@
 AddCSLuaFile()
 
-ENT.Base 			= "base_nextbot"
-ENT.Spawnable		= true
+ENT.Base = "base_nextbot"
+ENT.Spawnable = true
 
+-- Initialize the NPC
 function ENT:Initialize()
-	self:SetModel( "models/player/zombie_classic.mdl" )
-	self.health = 100
+    self:SetModel("models/player/zombie_classic.mdl")
+    self.health = 100
 
-	if SERVER then
-		self.loco:SetStepHeight( 18 )
-	end
+    -- Set the step height for the NPC's movement
+    if SERVER then
+        self.loco:SetStepHeight(18)
+    end
 
-	self.loco:SetDeathDropHeight( 1000 )	
+    -- Set the maximum height from which the NPC can drop to its death
+    self.loco:SetDeathDropHeight(1000)
 
-	self.loco:SetAcceleration( 900 )	
-	self.loco:SetDeceleration( 1200 )	
+    -- Set the acceleration and deceleration of the NPC's movement
+    self.loco:SetAcceleration(900)
+    self.loco:SetDeceleration(1200)
 
-	self.loco:SetJumpHeight( 58 )	
+    -- Set the jump height of the NPC
+    self.loco:SetJumpHeight(58)
 
-	self.loco:SetMaxYawRate( 360 )	
-	self.loco:SetDesiredSpeed( 100 )	
+    -- Set the maximum yaw rate of the NPC's movement
+    self.loco:SetMaxYawRate(360)
 
-	self.loco:SetGravity( 1 )		
+    -- Set the desired speed of the NPC's movement
+    self.loco:SetDesiredSpeed(100)
 
-	self:SetCollisionBounds( Vector(-16,-16,0), Vector(16,16,72) )
+    -- Set the gravity of the NPC
+    self.loco:SetGravity(1)
 
-	self:SetHealth( self.health )
+    -- Set the collision bounds of the NPC
+    self:SetCollisionBounds(Vector(-16, -16, 0), Vector(16, 16, 72))
 
-	self.loco:SetDeathDropHeight( 1000 )
+    -- Set the health of the NPC
+    self:SetHealth(self.health)
 
-	if SERVER then
-		self:SetMaxHealth( self.health )
-	end
+    -- Set the maximum health of the NPC on the server
+    if SERVER then
+        self:SetMaxHealth(self.health)
+    end
 
-	self.NextIdleSoundTime = CurTime() + math.random( 10, 20 )
+    -- Set the time until the NPC makes an idle sound
+    self.NextIdleSoundTime = CurTime() + math.random(10, 20)
 end
 
-function ENT:OnInjured( dmginfo )
-	self.health = self.health - dmginfo:GetDamage()
-	self:SetHealth( self.health )
+-- Handle what happens when the NPC is injured
+function ENT:OnInjured(dmginfo)
+    -- Decrement the NPC's health by the amount of damage inflicted
+    self.health = self.health - dmginfo:GetDamage()
+    self:SetHealth(self.health)
 
-	if self.health <= 0 then
-		self:OnKilled()
-	end
+    -- If the NPC's health reaches zero or less, handle its death
+    if self.health <= 0 then
+        self:OnKilled()
+    end
 end
 
+-- Handle what happens when the NPC is killed
 function ENT:OnKilled()
-	self:BecomeRagdoll( dmginfo )
-end
-
-function ENT:RunBehaviour()
-	while true do
-		if self.enemy then
-			self:ChaseEnemy()
-		else
-			self:SearchEnemy()
-		end
-
-		coroutine.yield()
-	end
-end
-
-function ENT:SearchEnemy()
-	local targets = ents.FindInSphere( self:GetPos(), self.SearchRadius )
-	local enemy = nil
-	local dist = self.LoseTargetDist
-
-	for k, v in pairs( targets ) do
-		if v:IsPlayer() and v:Alive() then
-			local thisDist = self:GetPos():DistToSqr( v:GetPos() )
-
-			if thisDist < dist then
-				enemy = v
-				dist = thisDist
-			end
-		end
-	end
-
-	if enemy then
-		self.enemy = enemy
-	end
-
-	coroutine.wait( 1 )
-end
-
-function ENT:ChaseEnemy()
-	self.loco:FaceTowards( self.enemy:GetPos() )
-
-	if self:GetRangeTo( self.enemy:GetPos() ) > self.LoseTargetDist then
-		self.enemy = nil
-		return
-	end
-
-	if self:CanAttack( self.enemy ) then
-		self:AttackEnemy()
-	else
-		self:MoveToEnemy()
-	end
-end
-
-function ENT:MoveToEnemy()
-	self.loco:FaceTowards( self.enemy:GetPos() )
-	self.loco:SetDesiredSpeed( 200 )
-
-	self:PlayIdleSound()
-
-	local path = Path( "Follow" )
-	path:SetMinLookAheadDistance( 300 )
-	path:SetGoalTolerance( 20 )
-	path:Compute( self, self.enemy:GetPos() )
-
-	if not path:IsValid() then return end
-
-	while path:IsValid() and self.enemy do
-		if self:CanAttack( self.enemy ) then
-			self:AttackEnemy()
-			return
-		end
-
-		path:Update( self )
-
-		if self:GetRangeTo( self.enemy:GetPos() ) < 200 then
-			self.loco:SetDesiredSpeed( 0 )
-		else
-			self.loco:SetDesiredSpeed( 200 )
-		end
-
-		coroutine.yield()
-	end
-end
-
-function ENT:CanAttack( ent )
-    -- If the enemy is not a player or nextbot, then we can't attack it
-    if not (ent:IsPlayer() or ent:GetClass():StartWith("nextbot")) then
-        return false
-    end
-
-    -- If the enemy is dead or not valid, then we can't attack it
-    if not IsValid(ent) or not ent:Alive() then
-        return false
-    end
-
-    -- If the enemy is too far away, then we can't attack it
-    if self:GetRangeTo(ent) > self.AttackRange then
-        return false
-    end
-
-    -- If we're not facing the enemy, then we can't attack it
-    local targetDir = (ent:GetPos() - self:GetPos()):GetNormalized()
-    local facingDir = self:GetForward()
-    if targetDir:Dot(facingDir) < 0.5 then
-        return false
-    end
-
-    return true
-end
-
-function ENT:OnInjured( dmginfo )
-    self:SetHealth(self:Health() - dmginfo:GetDamage())
-    if self:Health() <= 0 then
-        self:OnKilled(dmginfo:GetAttacker(), dmginfo:GetInflictor())
-    end
-end
-
-function ENT:OnKilled( attacker, inflictor )
-    self:SetModel("models/XQM/Rails/gumball_1.mdl") -- change model on death
-    self:SetCollisionGroup(COLLISION_GROUP_WEAPON) -- disable collision
+    -- Create a ragdoll of the NPC
     self:BecomeRagdoll(dmginfo)
-    timer.Simple(10, function() self:Remove() end) -- remove after some time
 end
 
+-- Handle the NPC's behavior
 function ENT:RunBehaviour()
+    -- Loop indefinitely
     while true do
-        if not self.IsAlerted then -- if not alerted, patrol
-            self:StartActivity(ACT_WALK)
-            self.loco:SetDesiredSpeed(50)
-            self:Patrol()
-        else -- if alerted, chase and attack
-            local enemy = self:GetEnemy()
-            if not IsValid(enemy) then -- if enemy is not valid, search for one
-                self:SearchEnemy()
-            else
-                if self:CanAttack(enemy) then -- if enemy is valid and within range, attack
-                    self:StartActivity(ACT_MELEE_ATTACK1)
-                    self.loco:SetDesiredSpeed(0)
-                    self:FaceEnemy()
-                    self:AttackEnemy()
-                else -- if enemy is valid but not within range, chase
-                    self:StartActivity(ACT_RUN)
-                    self.loco:SetDesiredSpeed(200)
-                    self:ChaseEnemy()
-                end
-            end
+        -- If the NPC has an enemy, chase it
+        if self.enemy then
+            self:ChaseEnemy()
+        -- If the NPC doesn't have an enemy, search for one
+        else
+            self:SearchEnemy()
         end
+
+        -- Yield control until the next frame
         coroutine.yield()
     end
+end
+
+-- Search for an enemy within the NPC's search radius
+function ENT:SearchEnemy()
+    -- Find all players within our vision cone
+    local targets = ents.FindInCone(self:GetPos(), self:GetForward(), self.SearchDistance, self.SearchAngle)
+
+    local nearestTarget = nil
+    local nearestTargetDistance = math.huge
+
+    for _, target in pairs(targets) do
+        if target:IsPlayer() and target:Alive() then
+            -- Calculate the distance to the target
+            local distanceToTarget = self:GetPos():DistToSqr(target:GetPos())
+
+            -- Check if the target is the nearest we've seen so far
+            if distanceToTarget < nearestTargetDistance then
+                nearestTarget = target
+                nearestTargetDistance = distanceToTarget
+            end
+        end
+    end
+
+    if nearestTarget then
+        self:SetEnemy(nearestTarget)
+        self:AlertAllies(nearestTarget)
+
+        -- Call OnFoundEnemy callback if it exists
+        if self.OnFoundEnemy then
+            self:OnFoundEnemy(nearestTarget)
+        end
+    end
+
+    return nearestTarget
 end
